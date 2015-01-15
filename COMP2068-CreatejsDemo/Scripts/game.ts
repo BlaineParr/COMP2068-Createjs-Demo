ae24810e332cae1dc00bed30c0dca11937fe1386
BlaineParr <Blaine.parr@hotmail.com>
2015-01-15 12:41:23 -0500

    Createjs Site structure

---
+++ COMP2068-CreatejsDemo/Scripts/game.ts
@@ -0,0 +1,19 @@
+﻿var canvas;
+var stage: createjs.Stage;
+
+function init() {
+    canvas = document.getElementById("game");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+} //function init ends
+
+function gameLoop() {
+    stage.update();
+} //function gameLoop ends
+
+function main() {
+    var helloText = new createjs.Text("Hello World!", "40px Arial", "#000000");
+    stage.addChild(helloText);
+} //funtion main ends
